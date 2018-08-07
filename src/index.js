6fe2e6c17bdf7d50cd539d836910eacb7f4db256
gongtiexin <tiexin.gong@qq.com>
2018-08-07 10:13:18 +0800

    路由懒加载

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from "vue";
 import moment from "moment";
 import App from "./App";
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import Router from "vue-router";
-import HelloWorld from "../components/HelloWorld";
 
 Vue.use(Router);
 
@@ -10,7 +9,8 @@ export default new Router({
     {
       path: "/",
       name: "HelloWorld",
-      component: HelloWorld,
+      component: () =>
+        import(/* webpackChunkName: "route-HelloWorld" */ "../components/HelloWorld"),
     },
   ],
 });
